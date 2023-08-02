976939c2359f09bb00671c86f78f5cbc1c250656
Yukai Huang <yukaihuangtw@gmail.com>
2023-08-03 00:28:56 +0800

    feat: use maplibre-gl directly in vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,33 @@
 
   <p>Mouse position: {{ x }}, {{ y }}</p>
 
-  <mgl-map style="height: 400px;"></mgl-map>
+  <div ref="mapRef" style="height: 400px"></div>
 </template>
 
 <script lang="ts">
-import toggleTheme from '~/hooks/toggleTheme'
-import { useMouse } from '@vueuse/core'
+import toggleTheme from "~/hooks/toggleTheme";
+import { useMouse } from "@vueuse/core";
+import { Map } from "maplibre-gl";
+import { onMounted, ref } from "vue";
 
 export default {
   setup() {
+    const mapRef = ref<HTMLDivElement>(null);
+
+    onMounted(() => {
+      const map = new Map({
+        container: mapRef.value!,
+        style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
+        center: [0, 0],
+        zoom: 2,
+      });
+    });
+
     return {
       ...toggleTheme(),
       ...useMouse(),
-    }
-  }
-}
+      mapRef,
+    };
+  },
+};
 </script>
